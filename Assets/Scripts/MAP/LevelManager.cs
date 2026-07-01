dbfd701ca5d35a830c93819d4a4c51dbc140a02c
florea-mihai-alexandru <mihai.florea1@student.usv.ro>
2026-07-01 22:28:04 +0300

    hitbox fixed

--- Assets/Scripts/MAP/LevelManager.cs
+++ Assets/Scripts/MAP/LevelManager.cs
@@ -10,15 +10,26 @@ public class LevelManager : MonoBehaviour
 
     public Room CurrentRoom { get; private set; }
 
-    public GameObject player;
 
     public int currentStage = 0;
 
+    [Header("UI")]
+    public GameObject deathScreen;
+    public GameObject successScreen;
+
+    [Header("Player")]
+    public GameObject player;
     public Transform playerTransform;
+    public Transform currentCheckpoint;
+
+    private bool transitionRunning = false;
 
     private void Awake()
     {
         Instance = this;
+
+        //deathScreen.SetActive(false);
+        //successScreen.SetActive(false);
     }
 
     public void EnterRoom(Room room)
@@ -48,4 +59,46 @@ public class LevelManager : MonoBehaviour
     {
         currentStage++;
     }
+
+    //public void PlayerDied()
+    //{
+    //    if (!transitionRunning)
+    //        StartCoroutine(DeathRoutine());
+    //}
+
+    //IEnumerator DeathRoutine()
+    //{
+    //    transitionRunning = true;
+
+    //    deathScreen.SetActive(true);
+
+    //    yield return new WaitForSeconds(2f);
+
+    //    player.position = currentCheckpoint.position;
+
+    //    deathScreen.SetActive(false);
+
+    //    transitionRunning = false;
+    //}
+
+    //public void LevelCompleted(Transform nextCheckpoint)
+    //{
+    //    if (!transitionRunning)
+    //        StartCoroutine(LevelCompleteRoutine(nextCheckpoint));
+    //}
+
+    //IEnumerator LevelCompleteRoutine(Transform nextCheckpoint)
+    //{
+    //    transitionRunning = true;
+
+    //    successScreen.SetActive(true);
+
+    //    yield return new WaitForSeconds(2f);
+
+    //    currentCheckpoint = nextCheckpoint;
+
+    //    successScreen.SetActive(false);
+
+    //    transitionRunning = false;
+    //}
 }
